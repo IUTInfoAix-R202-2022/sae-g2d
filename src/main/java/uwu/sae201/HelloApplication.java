d75e8f1f19e2c375bda64d49584227dc68cc6ce9
Sloane <sloane.metayer@sfr.fr>
2022-05-24 08:40:55 +0200

    Finalisation de la page de connexion administrateur

--- src/main/java/uwu/sae201/HelloApplication.java
+++ src/main/java/uwu/sae201/HelloApplication.java
@@ -10,9 +10,9 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("connexion_administrateur.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1920, 1080);
-        stage.setTitle("Hello!");
+        stage.setTitle("Connexion administrateur");
         stage.setScene(scene);
         stage.show();
     }
