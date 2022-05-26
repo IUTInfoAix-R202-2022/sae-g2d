c177e7207768755ce8d01928963b05ef9762e765
Sloane <sloane.metayer@sfr.fr>
2022-05-26 16:22:59 +0200

    Création de la page catégorie

--- src/main/java/uwu/sae201/HelloApplication.java
+++ src/main/java/uwu/sae201/HelloApplication.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("parametres.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("categorie.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1920, 1080);
         stage.setTitle("Dico Pédago");
         stage.setScene(scene);
