017a4892aac921cc5e42d4994fabf241b9f1f068
GuillaumeKusiak <guillaume.kusiak@etu.univ-amu.fr>
2022-05-26 16:17:33 +0200

    modification de fxml

--- src/main/java/uwu/sae201/HelloApplication.java
+++ src/main/java/uwu/sae201/HelloApplication.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Carte.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("parametre.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
         stage.setTitle("Dico Pédago");
         stage.setScene(scene);
