0809def5caa8a067e1df75de3eb5316bde50b272
SloaneMétayer <91048574+sloaneMetayer@users.noreply.github.com>
2022-05-30 17:35:02 +0200

    Update HelloApplication.java

--- src/main/java/uwu/sae201/HelloApplication.java
+++ src/main/java/uwu/sae201/HelloApplication.java
@@ -1,24 +1,35 @@
-package uwu.sae201;
+package fr.univ_amu.iut;
 
+import fr.univ_amu.iut.database.Database;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.sql.Connection;
 
 public class HelloApplication extends Application {
+
+    private static Connection connection;// On établie la connexion
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("ajouter_une_categorie.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1920, 1080);
+        connection = Database.getDBConnection();
+        Parent root = FXMLLoader.load(getClass().getResource("academie.fxml"));
+        Scene scene = new Scene(root, 1280, 720);
         stage.setTitle("Dico Pédago");
+        stage.setMaximized(true);
         stage.setScene(scene);
         stage.show();
     }
 
     public static void main(String[] args) {
         launch();
+    }
+
+    public static Connection getDBConnection() {
+        return connection;
     }
 }
